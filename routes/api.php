dcc0d6eb146628a0956cb1fd465af0ecae91ae94
apple <muhammedjishad7034@gmail.com>
2024-10-23 20:39:36 +0530

    update using patch

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,9 @@ use App\Http\Controllers\Api\V1\TaskController;
 */
 Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
